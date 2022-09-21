efd06a7b34514065ad4d87cdca2f3d768534857a
Arc9067 <80489007+Arc9067@users.noreply.github.com>
2022-09-21 21:22:12 +0000

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+// function myFunction() {
+//    var element = document.body;
+//    element.classList.toggle("dark-mode");
+//    var change = document.getElementById("toggle")
+//    change.classList.replace("fa-solid fa-toggle-on","fa-regular fa-toggle-large-off")
+// //    change.classList.toggle("fa-regular fa-toggle-large-off")
+// }
